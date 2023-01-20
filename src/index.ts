88873d6341622ef1cb0ad19a269518b9f676dc75
carlos <chlouzada@gmail.com>
2023-01-20 10:59:38 -0300

    swap db

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,52 @@
+import { PrismaClient } from '@prisma/client';
 import { connect } from 'mongoose';
-import { ExtractionModel } from './models/extraction';
 import puppeteer from 'puppeteer';
 
 require('dotenv').config();
 
-connect(process.env.DB!, async (err) => {
-  if (err) {
-    console.log('error connecting to db: ', err);
-    process.exit(1);
-  }
-
-  console.log('connected to db');
-
-  main();
-});
-
-
 const main = async () => {
   console.log('running');
+
+  const prisma = new PrismaClient();
+
+  const extractors = await prisma.extractor.findMany({
+    include: {
+      selectors: true,
+    },
+  });
+
+  console.log(extractors);
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  const extractions = await ExtractionModel.find({});
-
-  console.log(extractions);
-
-  for (const extraction of extractions) {
-    await page.goto(extraction.url);
+  for (const extractor of extractors) {
+    await page.goto(extractor.url);
     const values = [];
-    for (const selector of extraction.selectors) {
+    for (const selector of extractor.selectors) {
       const value = await page
         .evaluate(
-          (selector) => document.querySelector(selector)?.textContent,
+          (selector) => document.querySelector(selector.selector)?.textContent,
           selector
         )
         .catch((err) => console.log(err));
       values.push(value);
     }
-    extraction.extracted.push({
-      at: new Date(),
-      value: values.join(' '),
+
+    // create many results
+
+    await prisma.result.createMany({
+      data: values.map((value, index) => ({
+        value: value || null,
+        extractorId: extractor.id,
+        selectorId: extractor.selectors[index].id,
+      })),
     });
-    await extraction.save();
   }
 
   await browser.close();
 
   console.log('done');
 };
+
+main();
